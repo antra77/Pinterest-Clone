19c73247dba0b83c8cc84c968a9756be09542034
Antra Saraf <antra.saraf@gmail.com>
2024-04-05 16:59:09 +0530

    updated user profile page and header

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
+require('dotenv').config(); // Load environment variables from .env file
 
-mongoose.connect("mongodb://127.0.0.1:27017/pin");
+mongoose.connect(process.env.MONGODB_URI);
 
 const userSchema = mongoose.Schema({
   username: String,
